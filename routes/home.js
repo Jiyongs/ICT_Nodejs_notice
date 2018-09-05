143f5a8dc006022861a4988e4bf21e574bbc6d63
Jiyongs <shzy232@naver.com>
2018-09-05 10:16:56 +0900

    commit 1 (nav 메뉴 설정)

---
+++ routes/home.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router  = express.Router();
+
+// Home
+router.get("/", function(req, res){
+  res.render("home/welcome");
+});
+router.get("/about", function(req, res){
+  res.render("home/about");
+});
+// 추가
+router.get("/board", function(req, res){
+  res.render("home/board");
+});
+
+module.exports = router;
