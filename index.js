877799aefb3037d06f682cb93065ac5e94e94ebf
Jiyongs <shzy232@naver.com>
2018-09-07 16:32:17 +0900

    commit 4 (가입 입력 정보 제한 주기 & 로그인 기능 추가)

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ var express        = require("express");
 var mongoose       = require("mongoose");
 var bodyParser     = require("body-parser");
 var methodOverride = require("method-override");
+var flash = require("connect-flash");
+var session = require("express-session");
+var passport = require("./config/passport");
 var app = express();
+
 
 // DB setting
 mongoose.connect("mongodb://localhost:27017/ict_w1", { useNewUrlParser: true });
+mongoose.set ( 'useCreateIndex', true); // **mongoose 버전에 따른 오류로 추가한 것
 var db = mongoose.connection;
 db.once("open", function(){
   console.log("DB connected");
@@ -16,17 +21,35 @@ db.on("error", function(err){
   console.log("DB ERROR : ", err);
 });
 
+
 // Other settings
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.use(flash());
+app.use(session({secret:"MySecret", resave:true, saveUninitialized:true}));
+
+
+// Passport
+app.use(passport.initialize());  //passport의 초기화
+app.use(passport.session());     //passport를 session과 연결
+
+
+// Custom Middlewares
+app.use(function(req,res,next){
+  res.locals.isAuthenticated = req.isAuthenticated();  //로그인 여부 확인(t/f)
+  res.locals.currentUser = req.user;
+  next();
+ })
+
 
 // Routes
 app.use("/", require("./routes/home"));        //메인 화면 띄우기 라우터
 app.use("/posts", require("./routes/posts"));  //게시물 CRUD 관련 라우터
 app.use("/users", require("./routes/users"));  //회원가입 라우터
+
 
 // Port setting
 var port = 3000
--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,7 @@
 
 var express = require("express");
 var router  = express.Router();
+var passport = require("../config/passport")
 
 // Home
 router.get("/", function(req, res){
@@ -11,9 +12,51 @@ router.get("/", function(req, res){
 router.get("/about", function(req, res){
   res.render("home/about");
 });
-// 추가
 router.get("/board", function(req, res){
   res.render("home/board");
+});
+
+// Login
+router.get("/login", function (req,res) {
+  var username = req.flash("username")[0];
+  var errors = req.flash("errors")[0] || {};
+  res.render("home/login", {
+   username:username,
+   errors:errors
+  });
+ });
+
+ // Post Login (로그인 뷰용 라우터)
+router.post("/login",
+function(req,res,next){
+ var errors = {};
+ var isValid = true;
+ if(!req.body.username){
+  isValid = false;
+  errors.username = "ID를 입력해주세요!";
+ }
+ if(!req.body.password){
+  isValid = false;
+  errors.password = "비밀번호를 입력해주세요!";
+ }
+
+ if(isValid){
+  next();
+ } else {
+  req.flash("errors",errors);
+  res.redirect("/login");
+ }
+},
+passport.authenticate("local-login", {
+ successRedirect : "/",
+ failureRedirect : "/login"
+}
+));
+
+// Logout (로그아웃용 라우터)
+router.get("/logout", function(req, res) {
+req.logout();
+res.redirect("/");
 });
 
 module.exports = router;
